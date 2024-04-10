e2edf6edfd02852a5739744f2cef3dfbb2be0ada
Michal901 <momot1122@wp.pl>
2024-04-10 13:49:58 +0200

    .

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,8 +1,12 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+const StyledH2 = styled.h2`
+  display: flex;
+  justify-content: center;
+`;
 export default function Feedback({ message }) {
-  return <h2 className="feedback-msg">{message}</h2>;
+  return <StyledH2 className="feedback-msg">{message}</StyledH2>;
 }
 
 Feedback.propTypes = {
